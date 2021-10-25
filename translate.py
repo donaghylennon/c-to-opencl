8993d4471f6a0ba2c09ed430a1db79d04089289f
Lennon Donaghy <donaghylennon@gmail.com>
2021-10-25 02:09:03 +0100

    Add very basic translation of for loops into parallel OpenCL
    - Assumes that the loop can be directly translated to parallel OpenCL
      equivalents
    - Assumes indexes are initialised to zero in original code and that they
      were incremented by one each time
    - Simply inverts the original condition to form a guard
    - Assumes the loop body can be included unchanged

--- translate.py
+++ translate.py
@@ -1,4 +1,4 @@
-from pycparser import c_ast
+from pycparser import c_ast, c_generator
 
 def translate_function(ast):
     output = ""
@@ -11,7 +11,11 @@ def translate_function(ast):
     for i, param in enumerate(func_def.decl.type.args):
         if i != 0: output += ", "
         output += translate_declaration(param.type) + " " + param.name
-    output += ")"
+    output += ") {\n"
+
+    for_loop = func_def.body.block_items[0]
+    output += translate_for(for_loop)
+    output += "}\n"
 
     return output
 
@@ -23,4 +27,24 @@ def translate_declaration(node):
         output += "*"
     elif type(node) == c_ast.TypeDecl:
         output += " ".join(node.type.names)
+    return output
+
+def translate_for(node):
+    output = ""
+    indexes = []
+    for decl in node.init:
+        indexes.append(decl.name)
+
+    for i, index in enumerate(indexes):
+        output += f"int {index} = get_global_id({i});\n"
+
+    cond = node.cond
+    output += "if(!("
+    output += cond.left.name + " " + cond.op + " " + cond.right.name + "))\n"
+    output += "return;\n"
+
+    generator = c_generator.CGenerator()
+    for stmt in node.stmt:
+        output += generator.visit(stmt) + ";\n"
+
     return output
