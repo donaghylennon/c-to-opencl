1f02bd8d441b3cd6b9b5fb6e516effc8002acb07
Lennon Donaghy <donaghylennon@gmail.com>
2021-11-07 21:39:14 +0000

    Update example input file with code for host side to initilize arrays
    Now the assumption is that the first function in the file is the
    function to be translated into a kernel, with the second function being
    main, which is to be left as-is apart from the call to the kernel
    function.

--- Examples/AddArraysHost.c
+++ Examples/AddArraysHost.c
@@ -1,5 +1,18 @@
+#define BUF_SZ 100
+
 void AddArrays(int *a, int *b, int *c, int num_elements) {
     for (int i = 0; i < num_elements; i++) {
         a[i] = b[i] + c[i];
     }
+}
+
+int main() {
+    int a[BUF_SZ], b[BUF_SZ], c[BUF_SZ];
+
+    for (int i = 0; i < BUF_SZ; i++) {
+        a[i] = i;
+        b[i] = BUF_SZ - i;
+    }
+
+    AddArrays(a, b, c, BUF_SZ);
 }
