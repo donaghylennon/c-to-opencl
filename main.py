71c8ad3dc6d488f45af87414586baba3cce98908
Lennon Donaghy <donaghylennon@gmail.com>
2021-11-15 19:24:44 +0000

    Generate host code, and write host code and kernel to file
    Now we write both host code and kernel to file instead of printing,
    using new program arguments to give paths to write these to.

--- main.py
+++ main.py
@@ -1,16 +1,25 @@
 import argparse
 import pycparser
 
-import translate, host
+import translate
+import host
+
 
 def main():
     argparser = argparse.ArgumentParser()
-    argparser.add_argument('filepath', help='path to c file to translate')
+    argparser.add_argument('input_file', help='path to c file to translate')
+    argparser.add_argument('output_file', help='path to write c file containing host code')
+    argparser.add_argument('kernel_file', help='path to write cl file containing kernel code')
     args = argparser.parse_args()
 
-    ast = pycparser.parse_file(args.filepath, use_cpp=True)
+    ast = pycparser.parse_file(args.input_file, use_cpp=True)
     cl_output = translate.translate_function(ast)
-    print(cl_output)
+    with open(f'{args.kernel_file}', 'w') as f:
+        f.write(cl_output)
+    host_details = host.HostDetails.from_ast(ast)
+    host_code = host_details.generate_code()
+    with open(f'{args.output_file}', 'w') as f:
+        f.write(host_code)
 
 
 if __name__ == "__main__":
