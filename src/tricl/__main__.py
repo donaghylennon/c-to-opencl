98a948f5b27a2d8241701f962f5f28d8d7b00e5e
Lennon Donaghy <donaghylennon@gmail.com>
2022-01-28 01:33:18 +0000

    Use pycparser's fake libc, and allow passing custom include paths
    The fake libc allows using standard library includes without GCC
    extensions messing with pycparser. Using the -I option you can pass
    include paths to the C preprocessor.

--- src/tricl/__main__.py
+++ src/tricl/__main__.py
@@ -7,12 +7,16 @@ from . import host
 
 def main():
     argparser = argparse.ArgumentParser(prog="tricl")
+    argparser.add_argument('-I', help='c include path', action="append")
     argparser.add_argument('input_file', help='path to c file to translate')
     argparser.add_argument('output_file', help='path to write c file containing host code')
     argparser.add_argument('kernel_file', help='path to write cl file containing kernel code')
     args = argparser.parse_args()
 
-    ast = pycparser.parse_file(args.input_file, use_cpp=True)
+    cpp_args = ["-Iutils/fake_libc_include"] + ["-I" + path for path in args.I]
+    print(cpp_args)
+
+    ast = pycparser.parse_file(args.input_file, use_cpp=True, cpp_args=cpp_args)
     visitor = translate.Translator()
     cl_output = visitor.visit(ast)
     with open(args.kernel_file, 'w') as f:
